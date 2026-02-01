f8afc81ac09bdc710698e13c817b481d9604f15e
Amir <amirtwilc@gmail.com>
2026-02-01 20:03:32 +0100

    add cloud profile and connect to confluent and Supabase

--- backend/src/main/java/com/amir/mediatracker/config/KafkaConfig.java
+++ backend/src/main/java/com/amir/mediatracker/config/KafkaConfig.java
@@ -1,25 +1,11 @@
 package com.amir.mediatracker.config;
 
-import com.amir.mediatracker.kafka.event.RatingEvent;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.LongDeserializer;
-import org.apache.kafka.common.serialization.LongSerializer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.*;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -28,6 +14,7 @@ public class KafkaConfig {
     private final KafkaTopicProperties kafkaTopicProperties;
 
     @Bean
+    @Profile("local")
     public NewTopic ratingTopic() {
         return TopicBuilder.name(kafkaTopicProperties.getMediaRatingTopic())
                 .partitions(kafkaTopicProperties.getTopicPartitions())
